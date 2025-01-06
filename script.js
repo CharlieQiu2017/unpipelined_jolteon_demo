99f98d4fd63728045374c15c6f0afa03ff93399c
Charlie Qiu <longfeiqiu2012@gmail.com>
2025-01-06 15:04:31 +0800

    basicc animation

--- script.js
+++ script.js
@@ -121,8 +121,11 @@ for (let i = 0; i < 4; ++i) {
   }
 }
 
+const delta = 1000;
+const latency = 10;
+
 function createMsgVert (sender, receiver, msg, net_st) {
-  if (sender == receiver) return;
+  if (sender === receiver) return;
 
   let start_cx = coords[links[sender * 4 + receiver][0]][0],
       start_cy = coords[links[sender * 4 + receiver][0]][1],
@@ -136,13 +139,51 @@ function createMsgVert (sender, receiver, msg, net_st) {
                      .attr ("fill", "orange");
 
   msgCirc.transition ()
-         .duration (1000)
+         .duration (delta)
          .attr ("cx", end_cx)
          .attr ("cy", end_cy)
          .on ("end", function () {
            msgCirc.remove ();
-           if (receiver != 3) net_st.node_states[receiver].handleMsg (msg);
+           if (receiver != 3) {
+	     let {new_msgs, timer_reset} = net_st.node_states[receiver].handleMsgLoop (msg);
+	     for (let i = 0; i < new_msgs.length; ++i) net_st.addMsg (new_msgs[i]);
+	     if (timer_reset) { resetTimer (receiver, net_st); }
+	   }
          });
+}
+
+/* For each honest node we create an arc representing the timer */
+
+let arc_centers = [
+  [marginLeft + nodeSize + 75, marginTop + nodeSize - 35],
+  [width - marginRight - nodeSize + 75, marginTop + nodeSize - 35],
+  [marginLeft + nodeSize + 75, height - marginBottom - nodeSize - 35]
+];
+
+let arc = d3.arc ();
+let arcs = [];
+
+for (let i = 0; i < 3; ++i) {
+  arcs.push (svg.append ("path")
+		.attr ("fill", "red")
+		.attr ("transform", "translate(" + arc_centers[i][0] + "," + arc_centers[i][1] + ")")
+		.attr ("d", arc ({ startAngle: 0, endAngle: 2 * Math.PI, innerRadius: 21, outerRadius: 24 }))
+	    );
+}
+
+function resetTimer (node, net_st) {
+  arcs[node].interrupt ();
+  arcs[node].transition ()
+	    .duration (delta * latency)
+	    .ease (d3.easeLinear)
+	    .attrTween ("d", function (d) { return function (t) { return arc ({ startAngle: 0, endAngle: 2 * Math.PI * (1 - t), innerRadius: 21, outerRadius: 24 }) }} )
+	    .on ("end", function () {
+	      let timeout = net_st.node_states[node].doTimeout ();
+	      net_st.addMsg (timeout);
+	      let {new_msgs, timer_reset} = net_st.node_states[node].handleMsgLoop (timeout);
+	      for (let i = 0; i < new_msgs.length; ++i) net_st.addMsg (new_msgs[i]);
+	      if (timer_reset) resetTimer (node, net_st);
+	    });
 }
 
 const svg_block =
@@ -190,7 +231,7 @@ container.append (setValButton.node ());
 container.append (gstButton.node ());
 
 function leader_at (r) {
-  return (r % 4);
+  return ((r - 1) % 4);
 }
 
 class message {
@@ -242,7 +283,8 @@ class leader_phase {
 }
 
 class honest_node {
-  constructor () {
+  constructor (id) {
+    this.id = id;
     this.round = 1;
     this.leader_phase = new leader_phase (0, null);
     /* 5 voter phases are defined
@@ -260,15 +302,336 @@ class honest_node {
     this.recv_timeouts = [];
     for (let i = 0; i < 4; ++i) {
       this.recv_votes[i] = null;
-      this.recv_ecaches[i] = null;
-      this.recv_mcaches[i] = null;
       this.recv_timeouts[i] = null;
     }
     /* Next value to propose when this node becomes leader */
     this.client_data = 0;
+
+    /* Flag for whether QC has been sent for current round, to prevent repetition */
+    this.qc_sent = false;
+    /* Max round for which a TC has been sent, to prevent repetition */
+    this.max_tc_sent = 0;
   }
 
+  canBuildCache () {
+    let count = 0;
+    for (let i = 0; i < 4; ++i) {
+      if (this.recv_votes[i] !== null) ++count;
+    }
+    return count >= 3;
+  }
+
+  canBuildTC () {
+    let count = 0;
+    for (let i = 0; i < 4; ++i) {
+      if (this.recv_timeouts[i] !== null && this.recv_timeouts[i].type === 6 && this.recv_timeouts[i].r >= this.round) ++count;
+    }
+    return count >= 3;
+  }
+
+  canPull () { return this.leader_phase.type === 1; }
+  canInvoke () { return this.leader_phase.type === 3; }
+  canPush () { return this.leader_phase.type === 5; }
+  canBuildQC () { return (! this.qc_sent) && this.leader_phase.type === 7; }
+
+  doBuildCache (new_msgs) {
+    if (this.leader_phase.type === 2 && this.leader_phase.msg.type === 9) {
+      let votes = [];
+      for (let i = 0; i < 4; ++i) {
+	if (this.recv_votes[i] !== null) votes.push (this.recv_votes[i]);
+      }
+      let cert = new message (3, this.id, this.leader_phase.msg.r, this.leader_phase.msg.content, votes);
+      this.leader_phase.type = 3;
+      this.leader_phase.msg = cert;
+      for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+      new_msgs.push (cert);
+    } else if (this.leader_phase.type === 4 && this.leader_phase.msg.type === 10) {
+      let votes = [];
+      for (let i = 0; i < 4; ++i) {
+	if (this.recv_votes[i] !== null) votes.push (this.recv_votes[i]);
+      }
+      let cert = new message (4, this.id, this.leader_phase.msg.r, this.leader_phase.msg.content, votes);
+      this.leader_phase.type = 5;
+      this.leader_phase.msg = cert;
+      for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+      new_msgs.push (cert);
+    } else if (this.leader_phase.type === 6 && this.leader_phase.msg.type === 11) {
+      let votes = [];
+      for (let i = 0; i < 4; ++i) {
+	if (this.recv_votes[i] !== null) votes.push (this.recv_votes[i]);
+      }
+      let cert = new message (5, this.id, this.leader_phase.msg.r, 0, votes);
+      this.leader_phase.type = 7;
+      this.leader_phase.msg = cert;
+      for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+      new_msgs.push (cert);
+    }
+  }
+
+  doBuildTC (new_msgs) {
+    /* Find the maximum round for which we can build a TC */
+    let max_r = this.round;
+    let test = true;
+    while (test) {
+      max_r++;
+      let count = 0;
+      for (let i = 0; i < 4; ++i) {
+	if (this.recv_timeouts[i] !== null && this.recv_timeouts[i].type === 6) {
+	  let timeout_log_time = 0;
+	  if (this.recv_timeouts[i].embedded.length > 0 && this.recv_timeouts[i].embedded[0].type === 4) {
+	    timeout_log_time = this.recv_timeouts[i].embedded[0].r;
+	  }
+	  if (this.recv_timeouts[i].r >= max_r && timeout_log_time <= max_r) ++count;
+	}
+      }
+      if (count < 3) test = false;
+    }
+
+    /* When loop exits, test = false, and the max round we can build TC is max_r - 1 */
+    max_r--;
+    if (max_r <= this.max_tc_sent) return;
+    this.max_tc_sent = max_r;
+
+    let timeouts = [];
+    let log_time = 0;
+    for (let i = 0; i < 4; ++i) {
+      if (this.recv_timeouts[i] !== null && this.recv_timeouts[i].type === 6) {
+	let timeout_log_time = 0;
+	if (this.recv_timeouts[i].embedded.length > 0 && this.recv_timeouts[i].embedded[0].type === 4) {
+	  timeout_log_time = this.recv_timeouts[i].embedded[0].r;
+	}
+	if (this.recv_timeouts[i].r >= max_r && timeout_log_time <= max_r) {
+	  timeouts.push (this.recv_timeouts[i]);
+	  if (timeout_log_time > log_time) log_time = timeout_log_time;
+	}
+      }
+    }
+
+    let tc = new message (7, this.id, max_r, log_time, timeouts);
+    new_msgs.push (tc);
+  }
+
+  doPull (new_msgs) {
+    if (this.leader_phase.type === 1 && (this.leader_phase.msg.type === 7 || this.leader_phase.msg.type === 8)) {
+      let log_time = 0;
+      if (this.leader_phase.msg.type === 7) log_time = this.leader_phase.msg.content; else log_time = this.leader_phase.msg.r;
+      let req = new message (9, this.id, this.round, log_time, [this.leader_phase.msg]);
+      this.leader_phase.type = 2;
+      this.leader_phase.msg = req;
+      new_msgs.push (req);
+    }
+  }
+
+  doInvoke (new_msgs) {
+    if (this.leader_phase.type === 3 && this.leader_phase.msg.type === 3) {
+      let req = new message (10, this.id, this.round, this.client_data, [this.leader_phase.msg]);
+      this.leader_phase.type = 4;
+      this.leader_phase.msg = req;
+      new_msgs.push (req);
+    }
+  }
+
+  doPush (new_msgs) {
+    if (this.leader_phase.type === 5 && this.leader_phase.msg.type === 4) {
+      let req = new message (11, this.id, this.round, 0, [this.leader_phase.msg]);
+      this.leader_phase.type = 6;
+      this.leader_phase.msg = req;
+      new_msgs.push (req);
+    }
+  }
+
+  doBuildQC (new_msgs) {
+    if (this.leader_phase.type === 7 && this.leader_phase.msg.type === 5) {
+      new_msgs.push (new message (8, this.id, this.round, 0, [this.leader_phase.msg]));
+      this.qc_sent = true;
+    }
+  }
+
+  doTimeout () {
+    this.voter_phase = 4;
+    let mc = [];
+    if (this.commit_round > 0) mc.push (this.recv_mcaches[this.commit_round]);
+    return new message (6, this.id, this.round, 0, mc);
+  }
+
+  /* Returns a list of new messages to send */
   handleMsg (msg) {
+    let new_msgs = [];
+
+    if (msg.type === 0) { /* ECacheVote */
+      if (this.leader_phase.type === 2 && this.leader_phase.msg.type === 9) {
+	if (this.leader_phase.msg.r === msg.r && this.leader_phase.msg.content === msg.content) {
+	  this.recv_votes[msg.nid] = msg;
+	}
+      }
+    } else if (msg.type === 1) { /* MCacheVote */
+      if (this.leader_phase.type === 4 && this.leader_phase.msg.type === 10) {
+	if (this.leader_phase.msg.r === msg.r && this.leader_phase.msg.content === msg.content) {
+	  this.recv_votes[msg.nid] = msg;
+	}
+      }
+    } else if (msg.type === 2) { /* CCacheVote */
+      if (this.leader_phase.type === 6 && this.leader_phase.msg.type === 11) {
+	if (this.leader_phase.msg.r === msg.r) {
+	  this.recv_votes[msg.nid] = msg;
+	}
+      }
+    } else if (msg.type === 6) { /* Timeout */
+      let timeout_log_time = 0;
+      if (msg.embedded.length > 0 && msg.embedded[0].type === 4) {
+	timeout_log_time = msg.embedded[0].r;
+      }
+      if (this.recv_timeouts[msg.nid] === null || (this.recv_timeouts[msg.nid].type === 6 && this.recv_timeouts[msg.nid].r < msg.r)) {
+	this.recv_timeouts[msg.nid] = msg;
+	if (this.round < timeout_log_time) {
+	  this.round = timeout_log_time;
+	  this.leader_phase.type = 0;
+	  this.leader_phase.msg = null;
+	  this.voter_phase = 0;
+	  for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+	  this.qc_sent = false;
+	}
+      }
+    } else if (msg.type === 7 || msg.type === 8) { /* TimeoutCert, CommitCert */
+      if (this.round <= msg.r) {
+	this.round = msg.r + 1;
+	if (this.id === leader_at (msg.r + 1)) {
+	  this.leader_phase.type = 1;
+	  this.leader_phase.msg = msg;
+	} else {
+	  this.leader_phase.type = 0;
+	  this.leader_phase.msg = null;
+	}
+	this.voter_phase = 0;
+	for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+	this.qc_sent = false;
+
+	if (msg.nid !== this.id && this.id !== leader_at (msg.r + 1)) {
+	  let forward_tc = new message (msg.type, this.id, msg.r, msg.content, msg.embedded);
+	  new_msgs.push (forward_tc);
+	}
+      }
+    } else if (msg.type === 9) { /* PullReq */
+      if (this.round < msg.r) {
+	this.round = msg.r;
+	this.leader_phase.type = 0;
+	this.leader_phase.msg = null;
+	this.voter_phase = 1;
+	this.qc_sent = false;
+	for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 7) {
+	  if (msg.embedded[0].content > 0) {
+	    for (let i = 0; i < msg.embedded[0].embedded.length; ++i) {
+	      if (msg.embedded[0].embedded[i].type === 6 &&
+		  msg.embedded[0].embedded[i].embedded.length > 0 &&
+		  msg.embedded[0].embedded[i].embedded[0].type === 4 &&
+		  msg.embedded[0].embedded[i].embedded[0].r === msg.embedded[0].content) {
+		this.recv_mcaches[msg.embedded[0].content] = msg.embedded[0].embedded[i].embedded[0];
+	      }
+	    }
+	  }
+	}
+	let vote = new message (0, this.id, msg.r, msg.content, []);
+	new_msgs.push (vote);
+      } else if (this.round === msg.r && this.voter_phase === 0) {
+	this.voter_phase = 1;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 7) {
+	  if (msg.embedded[0].content > 0) {
+	    for (let i = 0; i < msg.embedded[0].embedded.length; ++i) {
+	      if (msg.embedded[0].embedded[i].type === 6 &&
+		  msg.embedded[0].embedded[i].embedded.length > 0 &&
+		  msg.embedded[0].embedded[i].embedded[0].type === 4 &&
+		  msg.embedded[0].embedded[i].embedded[0].r === msg.embedded[0].content) {
+		this.recv_mcaches[msg.embedded[0].content] = msg.embedded[0].embedded[i].embedded[0];
+	      }
+	    }
+	  }
+	}
+	let vote = new message (0, this.id, msg.r, msg.content, []);
+	new_msgs.push (vote);
+      }
+    } else if (msg.type === 10) { /* InvokeReq */
+      if (this.round < msg.r) {
+	this.round = msg.r;
+	this.leader_phase.type = 0;
+	this.leader_phase.msg = null;
+	this.voter_phase = 2;
+	this.qc_sent = false;
+	for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 3) {
+	  this.recv_ecaches[msg.r] = msg.embedded[0];
+	}
+	let vote = new message (1, this.id, msg.r, msg.content, []);
+	new_msgs.push (vote);
+      } else if (this.round === msg.r && this.voter_phase >= 0 && this.voter_phase <= 1) {
+	this.voter_phase = 2;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 3) {
+	  this.recv_ecaches[msg.r] = msg.embedded[0];
+	}
+	let vote = new message (1, this.id, msg.r, msg.content, []);
+	new_msgs.push (vote);
+      }
+    } else if (msg.type === 11) { /* PushReq */
+      if (this.round < msg.r) {
+	this.round = msg.r;
+	this.leader_phase.type = 0;
+	this.leader_phase.msg = null;
+	this.voter_phase = 3;
+	this.commit_round = msg.r;
+	this.qc_sent = false;
+	for (let i = 0; i < 4; ++i) this.recv_votes[i] = null;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 4) {
+	  this.recv_mcaches[msg.r] = msg.embedded[0];
+	}
+	let vote = new message (2, this.id, msg.r, 0, []);
+	new_msgs.push (vote);
+      } else if (this.round === msg.r && this.voter_phase >= 0 && this.voter_phase <= 2) {
+	this.voter_phase = 3;
+	this.commit_round = msg.r;
+	if (msg.embedded.length > 0 && msg.embedded[0].type === 4) {
+	  this.recv_mcaches[msg.r] = msg.embedded[0];
+	}
+	let vote = new message (2, this.id, msg.r, 0, []);
+	new_msgs.push (vote);
+      }
+    }
+
+    if (this.canBuildCache ()) { this.doBuildCache (new_msgs); }
+    if (this.canPull ()) { this.doPull (new_msgs); }
+    if (this.canInvoke ()) { this.doInvoke (new_msgs); }
+    if (this.canPush ()) { this.doPush (new_msgs); }
+    if (this.canBuildQC ()) { this.doBuildQC (new_msgs); }
+    if (this.canBuildTC ()) { this.doBuildTC (new_msgs); }
+
+    return new_msgs;
+  }
+
+  /* Handling one message may cause the process to send out more messages,
+     and if the process itself is a recipient of the new message, then its
+     handler must be invoked immediately. Hence use a loop to process new
+     messages until no new message needs to be delivered to oneself.
+   */
+  handleMsgLoop (msg) {
+    let old_round = this.round, new_round = 0;
+
+    let msg_queue = [msg], curr_idx = 0;
+    let new_msgs = [];
+
+    while (curr_idx < msg_queue.length) {
+      let new_msg_tmp = this.handleMsg (msg_queue[curr_idx]);
+      curr_idx++;
+      for (let i = 0; i < new_msg_tmp.length; ++i) {
+	new_msgs.push (new_msg_tmp[i]);
+	if (new_msg_tmp[i].type >= 6 && new_msg_tmp[i].type <= 11) msg_queue.push (new_msg_tmp[i]);
+	if (new_msg_tmp[i].type >= 0 && new_msg_tmp[i].type <= 2 && this.id === leader_at (new_msg_tmp[i].r)) msg_queue.push (new_msg_tmp[i]);
+      }
+    }
+
+    new_round = this.round;
+    return {
+      new_msgs: new_msgs,
+      timer_reset : new_round > old_round
+    }
   }
 }
 
@@ -276,31 +639,36 @@ class net_state {
   constructor () {
     this.node_states = [];
     /* Only create node state for honest nodes */
-    for (let i = 0; i < 3; ++i) this.node_states[i] = new honest_node ();
+    for (let i = 0; i < 3; ++i) this.node_states[i] = new honest_node (i);
     this.msgs = [];
     this.undeliv_msg_list = null;
   }
 
   addMsg (msg) {
     this.msgs.push (msg);
-    msg.next_undeliv_msg = this.undeliv_msg_list;
-    if (this.undeliv_msg_list !== null) {
-      this.undeliv_msg_list.prev_undeliv_msg = msg;
+
+    if ((msg.type >= 0 && msg.type <= 2) || (msg.type >= 6 && msg.type <= 11)) {
+      msg.next_undeliv_msg = this.undeliv_msg_list;
+      if (this.undeliv_msg_list !== null) {
+	this.undeliv_msg_list.prev_undeliv_msg = msg;
+      }
+      this.undeliv_msg_list = msg;
+      /* For the moment, deliver the msg immediately */
+      this.delivMsgAll (msg);
     }
-    this.undeliv_msg_list = msg;
   }
 
   delivMsgOne (msg, receiver) {
-    createMsgVert (msg.nid, receiver, msg);
+    createMsgVert (msg.nid, receiver, msg, this);
   }
 
   delivMsgAll (msg) {
-    if (msg.type == 6 || msg.type >= 9) {
-      for (let i = 0; i < 4; ++i) createMsgVert (msg.nid, i, msg);
+    if (msg.type === 6 || msg.type >= 9) {
+      for (let i = 0; i < 4; ++i) createMsgVert (msg.nid, i, msg, this);
     } else if (msg.type <= 2) {
-      createMsgVert (msg,nid, leader_at (msg.r), msg);
-    } else if (msg.type == 7 || msg.type == 8) {
-      createMsgVert (msg,nid, leader_at (msg.r + 1), msg);
+      createMsgVert (msg.nid, leader_at (msg.r), msg, this);
+    } else if (msg.type === 7 || msg.type === 8) {
+      createMsgVert (msg.nid, leader_at (msg.r + 1), msg, this);
     }
     if (msg.next_undeliv_msg !== null) {
       msg.next_undeliv_msg.prev_undeliv_msg = msg.prev_undeliv_msg;
@@ -313,4 +681,11 @@ class net_state {
     msg.prev_undeliv_msg = null;
     msg.next_undeliv_msg = null;
   }
+}
+
+let global_net_st = new net_state ();
+document.global_net_st = global_net_st;
+
+for (let i = 0; i < 3; ++i) {
+  resetTimer (i, global_net_st);
 }
